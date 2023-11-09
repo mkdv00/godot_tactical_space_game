e4eac2085db4c029d0855d2cca764b7ad47cd64c
Maksim Kudaev <kudaev.m@promail366.com>
2023-11-09 23:23:56 +0300

    feat: Add Global Singleton and add Enum of physics layers

---
+++ TacticalSpaceCombat/Global.gd
@@ -0,0 +1,13 @@
+extends Node
+
+enum Layers { NONE }
+
+
+func _ready() -> void:
+	var label := 'layer_names/2d_physics/layer_%d'
+	
+	for i in range(20):
+		var layer_name: String = ProjectSettings.get_setting(label % (i + 1))
+		
+		if layer_name != '':
+			Layers[layer_name.to_upper()] = 1 << i
