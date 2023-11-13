a18e587fc05718edca3f81dfeb832c8a6dbdd47f
Maksim Kudaev <kudaev.m@promail366.com>
2023-11-13 23:47:38 +0300

    feat: Add ShipTemplate class and Add Rooms, Walls and Doors.

---
+++ TacticalSpaceCombat/Ship/Rooms/Room.gd
@@ -0,0 +1,54 @@
+tool
+class_name ShipRoom
+extends Area2D
+
+export var size := Vector2.ONE setget set_size
+
+var _area := 0
+var _top_left := Vector2.ZERO
+var _bottom_right := Vector2.ZERO
+
+var _iter_index := 0
+
+var _tilemap: TileMap = null
+
+onready var collision_shape: CollisionShape2D = $CollisionShape2D
+
+
+func setup(tilemap: TileMap) -> void:
+	_tilemap = tilemap
+	_setup_extents()
+
+	_area = size.x * size.y
+	_top_left = _tilemap.world_to_map(position - collision_shape.shape.extents)
+	_bottom_right = _top_left + size
+
+
+func _setup_extents() -> void:
+	if _tilemap != null:
+		collision_shape.shape.extents = 0.5 * _tilemap.map_to_world(size)
+
+
+func set_size(value: Vector2) -> void:
+	for axis in [Vector2.AXIS_X, Vector2.AXIS_Y]:
+		size[axis] = max(1, value[axis])
+	_setup_extents()
+
+
+func _iter_init(_arg) -> bool:
+	_iter_index = 0
+	return _iter_is_running()
+
+
+func _iter_next(_arg) -> bool:
+	_iter_index += 1
+	return _iter_is_running()
+
+
+func _iter_get(_arg) -> Vector2:
+	var offset := Utils.index_to_xy(size.x, _iter_index)
+	return _top_left + offset
+
+
+func _iter_is_running() -> bool:
+	return _iter_index < _area
---
+++ TacticalSpaceCombat/Ship/ShipTemplate.gd
@@ -0,0 +1,25 @@
+tool
+extends Node2D
+
+onready var tile_map: TileMap = $TileMap
+onready var rooms: Node2D = $Rooms
+
+
+func _ready() -> void:
+	call("_ready_editor_hint" if Engine.editor_hint else "_ready_not_editor_hint")
+
+
+func _ready_editor_hint() -> void:
+	for room in rooms.get_children():
+		if room is ShipRoom:
+			room.setup(tile_map)
+
+
+func _ready_not_editor_hint() -> void:
+	for room in rooms.get_children():
+		if room is ShipRoom:
+			room.setup(tile_map)
+			
+			for point in room:
+				tile_map.set_cellv(point, 0)
+	tile_map.update_bitmask_region()
